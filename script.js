77f8afed3c900d2392c98d45a431eb1b62dd3068
Makram <Mkelghadir@gmail.com>
2024-01-23 14:18:33 +0100

    newva

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("I   H A T E   K N I F E - E A R S")
+
+async function mi_funcion() {
+    var response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    console.log(response);
+  }
+
+mi_funcion()
